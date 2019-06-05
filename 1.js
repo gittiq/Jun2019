dcb6a4c1e5df1a8889e43022892833616b358c30
박경욱 <kyungwook@silver-27.local>
2019-06-05 18:32:51 +0900

    no message

---
+++ 1.js
@@ -0,0 +1,17 @@
+var client = require('cheerio-httpcli');
+
+var code = 'USDKRW=X';
+var url = 'http://finance.yahoo.com/q';
+
+client.fetch(url, { 's': code }, function(err, $, res) {
+    if (err) {
+        console.log('error');
+        return;
+    }
+    var str = $('#quote-header-info > div > div > div > span:nth-child(1)').text();
+    str = str.replace(/,/g, "");
+    var arr = str.match(/\d+[.]\d*/g);
+
+    console.log(code);
+    console.log(arr[0]);
+});
